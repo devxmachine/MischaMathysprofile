fbe8e158f7f2c87368acb23cf8b9bbda461adb6d
DevXmachine <106011808+devxmachine@users.noreply.github.com>
2022-10-05 16:40:22 +0200

    Change mousover JS

--- main.js
+++ main.js
@@ -2,15 +2,24 @@
 const longForm = document.getElementById('essay');
 const quote = document.getElementById('extract');
 longForm.style.display = 'none'; 
+function Change() {
+    quote.style.cursor = "pointer";
+    quote.style.color = "#FFE2E2";
+}
 function revealText () {
+    longForm.style.cursor = "pointer";
     quote.style.display ='none';
     longForm.style.display = 'block';
+    
 }
 function closeText (){
+    quote.style.cursor = "pointer";
     quote.style.display ='block';
     longForm.style.display = 'none';
 }
 
+quote.addEventListener('mouseover', Change);
 quote.addEventListener('click', revealText);
 longForm.addEventListener('click', closeText);
+
 
